7c95d4cedaec6ee1e98c2202423724b9a2e598db
nooblyf12345 <zubair18299@outlook.com>
2022-12-30 02:00:13 +0500

    trying to set up the ui

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const app =  express();
+http = require('http'),
+httpServer = http.Server(app);
 
-app.get('/',index())
-function index(req, resp) {
-    resp.send("Helloworld");
-}
+
+
+app.get('/', function(req,res) {
+    res.sendfile('templates/index.html');
+  });
 
 app.listen(3000, function() {
     console.log("listening on 3000");
